553d3737c73f74f002e234d36c1393c6a98acff2
Akanksha9392 <vepuriakanksha@gmail.com>
2025-08-06 17:16:49 +0530

    Add files via upload

---
+++ 3.2.py
@@ -0,0 +1,29 @@
+#print(5*2**10)
+#glass_of_water =20
+#glass_of_water= print("I drank", glass_of_water, "glasses of water today.") 
+
+#glass_of_water=3  
+#glass_of_water=glass_of_water + 1 
+#print(glass_of_water) 
+
+#men_stepped_on_the_moon = 9 
+#print(men_stepped_on_the_moon) 
+
+ 
+#my_reason_for_coding= "I want to become a python developer"
+#print(my_reason_for_coding) 
+
+#global_mean_sea_level_2018=21 
+#global_mean_sea_level_2018=19.01 
+#print(global_mean_sea_level_2018) 
+#staying_alive= True 
+#print(staying_alive) 
+#print("I am Akanksha,I am learning Python in Quality Thoughts") 
+#my_text="Hey Python Developer"
+#print(my_text) 
+#print("Hello World") 
+#x, y, z = 10, ‘Hello’, True 
+#x = 10,
+#y =’Hello’ 
+#z = True 
+
