5793f55bc8d0dbbea2a9bfb3bb69e4838104b42a
Akanksha9392 <vepuriakanksha@gmail.com>
2025-08-06 17:16:23 +0530

    Add files via upload

---
+++ worksheet3.py
@@ -0,0 +1,38 @@
+print (2 ** 3 ** 2) 
+print (2 * 3 ** 3 * 4) 
+print (10 - 4 * 2) 
+print (-18 // 4) 
+print (2%6) 
+x = 100  
+y = 50  
+print(x and y) 
+print(36 / 4) 
+var1 = 1  
+var2 = 2 
+var3 = 3 
+print (var1 + var2 + var3) 
+p, q, r = 10, 20 ,30
+print(p, q, r) 
+valueOne = 5 ** 2  
+valueTwo = 5 ** 3  
+print(valueOne) 
+print(valueTwo) 
+var = "James" * 2 * 3 
+print(var) 
+x= 36 / 4 * (3 + 2) * 4 + 2 
+print(x) 
+print(type (10)) 
+
+Age=21
+print(type(Age))
+
+Name="Akanksha"
+Age=21.0
+RollNumber=19
+print(Name,type(Name))
+print(Age,type(Age))
+print(RollNumber,type(RollNumber))
+
+
+
+
